840e2911bc9aa2b19e6928d0a9ff95caf77902e7
Julian Vega <ingjulianvega@gmail.com>
2021-07-26 08:37:16 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/occupation/web/model/Occupation.java
+++ src/main/java/ingjulianvega/ximic/occupation/web/model/Occupation.java
@@ -5,12 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Occupation {
 
+  @NotBlank
   private String name = null;
 
 }
